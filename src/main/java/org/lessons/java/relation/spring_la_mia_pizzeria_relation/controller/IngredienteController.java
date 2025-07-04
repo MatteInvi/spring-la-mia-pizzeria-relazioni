7544fe05ec195abb59dcad7294d326a98852b5f4
MatteInvi <matteoinvidia@gmail.com>
2025-07-04 09:29:58 +0200

    implementazione spring security

--- src/main/java/org/lessons/java/relation/spring_la_mia_pizzeria_relation/controller/IngredienteController.java
+++ src/main/java/org/lessons/java/relation/spring_la_mia_pizzeria_relation/controller/IngredienteController.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -54,6 +53,12 @@ public class IngredienteController {
 
        ingredientiRepository.save(formIngrediente);
        return "redirect:/ingredienti";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String edit(@PathVariable Integer id ,Model model){
+        model.addAttribute("ingrediente", ingredientiRepository.findById(id));
+        return "/ingredienti/edit";
     }
 
 }
